41aec16f6180cec0ce7e0ba2958e5f42f5ee8b3b
marcwolfe <marc.wolfe21@gmail.com>
2022-01-26 10:01:10 -0600

    users model and controller set up

---
+++ models/usersModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+
+const usersSchema = new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+})
+
+
+module.exports = model('User', usersSchema)
